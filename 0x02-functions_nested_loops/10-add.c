9f1eea8bb49e7f8c66379ed692aff5aba09c8adf
meecole <amyozegbe@gmail.com>
2023-06-21 14:18:14 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Adds two value
- * @n1 - The first number to be computed
- * @n2 - The second number to be computed
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
  * Return: Integer value
  */
 
